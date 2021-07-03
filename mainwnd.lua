4be80930e9d3855af34b94445bf85a02d5663a63
AnikiLulala <loctaa@gmail.com>
2021-07-03 23:59:43 +0800

    11

--- mainwnd.lua
+++ mainwnd.lua
@@ -7,9 +7,9 @@ local contextMenu=menu:addContextMenuItem("File")
 contextMenu:addItem("quit").onTouch=function()
     window:remove()
 end
-local vlist1=layout:setPosition(1,1,layout:addChild(GUI.list(3, 2, 25, 25, 2, 1, 0xFFFFFF, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false)))
+local vlist1=layout:setPosition(1,1,layout:addChild(GUI.list(3, 2, 25, 25, 2, 0, 0xFFFFFF, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false)))
 btn1=layout:setPosition(1, 2, layout:addChild(GUI.button(1, 1, 26, 3, 0xEEEEEE, 0x000000, 0xAAAAAA, 0x0, "Button 2")))
-local vlist2=layout:setPosition(2,1,layout:addChild(GUI.list(3, 2, 25, 30, 3, 0, 0xFFFFFF, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false)))
+local vlist2=layout:setPosition(2,1,layout:addChild(GUI.list(3, 2, 25, 25, 2, 0, 0xFFFFFF, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false)))
 for i=1,6 do
     vlist1:addItem("empty")
     vlist2:addItem("empty")
