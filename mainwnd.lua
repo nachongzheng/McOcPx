921d06f80a4a92e7a89e56572f6f79c534271018
AnikiLulala <loctaa@gmail.com>
2021-07-03 21:59:12 +0800

    1

--- mainwnd.lua
+++ mainwnd.lua
@@ -6,7 +6,11 @@ local contextMenu=menu:addContextMenuItem("File")
 contextMenu:addItem("quit").onTouch=function()
     window:remove()
 end
-
+local vlist1=layout:setPosition(1,1,layout:addChild(GUI.list(3, 2, 25, 30, 3, 0, 0xE1E1E1, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false)))
+vlist1:addItem("Hello world")
+vlist1:addItem("This is test").onTouch = function()
+	GUI.alert("Selected item: " .. verticalList.selectedItem)
+end
 
 window.onResize = function ( newWidth,newHeight )
     window.backgrondPanel.width,window.backgrondPanel.height=newWidth,newHeight
