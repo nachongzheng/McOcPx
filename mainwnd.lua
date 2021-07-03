c23e00e536b635a7a4e006cdaab1dbfdcd8229e7
AnikiLulala <loctaa@gmail.com>
2021-07-03 21:40:55 +0800

    1

--- mainwnd.lua
+++ mainwnd.lua
@@ -1,6 +1,6 @@
 local GUI = require("GUI")
 local system = require("System")
-local workspace,window,menu=system.addWindow(GUI.filledWindow(1,1,60,20,0xE1E1E1))
+local workspace,window,menu=system.addWindow(GUI.filledWindow(1,1,120,40,0xE1E1E1))
 local layout=window:addChild(GUI.layout(1,1,window.width,window.height,1,1))
 local contextMenu=menu:addContextMenuItem("File")
 contextMenu:addItem("quit").onTouch=function()
